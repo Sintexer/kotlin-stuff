df6f55b996fe5016cd3285076fea25e9e40103d5
Илья Буглаков <Neonlightnight@gmail.com>
2021-02-12 12:40:04 +0300

    add: isOdd() function; finished: main

--- odd-or-even/src/main/kotlin/main.kt
+++ odd-or-even/src/main/kotlin/main.kt
@@ -1,3 +1,14 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    var input = "";
+    val invitation = "Type a number or \"q\" to exit";
+    do {
+        println(invitation)
+        input = readLine()!!
+        input.toIntOrNull()?.also {
+            println("This number is ${if(isOdd(it)) "odd" else "even"}") }
+    }while (input != "q")
+}
+
+fun isOdd(num: Int): Boolean {
+    return num % 2 !=0
 }
